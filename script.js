ee093c1e68cef44673bb6662139cddc15a402e02
sattara1985 <sattara1985@gmail.com>
2023-05-24 19:08:10 -0500

    practica 1 de javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+document.write("Hola mundo X2");
+
+
+document.write('<h1 style="color:red">Bienvenidos a el repaso de JS</h1>');
+
+console.log("Hola mundo!")
+console.warn("mensajes en amarillo")
+console.error("mensaje error en rojo")
+//lenguaje interpretado es javascript
+alert("Hola mundo 3")
